f3ed546c5ca65375f111ad677b3692b3953a0a43
paolomp2 <p.munoz@pucp.pe>
2015-10-29 23:32:09 -0500

    Traps lunchbox and Mesh and zoom function fixed
    El traps lunch aún no se termina

--- src/app.js
+++ src/app.js
@@ -450,7 +450,7 @@ var childMoveAction = (function(){
                 return;
             }
         }
-
+        monstY+=clockController.getSpeed();
         mainLayer.sprite.setPosition(xNew,yNew);
         monstruo.setPosition(monstX,monstY);
     }
@@ -517,9 +517,15 @@ var GameplayMap = cc.TMXTiledMap.extend({
             },
 
             onKeyReleased: function(keyCode, event){
+
                 if(BoardController.isActivated() && keyCode>=65 && keyCode<=90){
                     var letter = String.fromCharCode(keyCode);
                     BoardController.keyboardInput(letter);
+                }
+
+                if(MeshController.isActivated()){
+
+                    MeshController.keyboardInput(keyCode);
                 }
             }
 
@@ -703,66 +709,35 @@ var zoomGame = {
 
     autoZoomIn:function()
     {
-          if(this.currentScale<1)
-          {
-              console.log(this.currentScale);
-              this.currentScale+=this.zoomRange;
-              gameplayMap.setScale(this.currentScale);
-              return true;
-          }else
-            return false;
-
-    },
-
-    //funcion que realiza el zoom sobre el mapa
-    zoomMap:function()
-    {
         if(this.zoomActivate)
         {
-            console.log("printing: "+this.timeLeft);
-            if(this.typeZoom == 0)            {
-                this.currentScale += this.zoomRange;
-            }else
-                this.currentScale -= this.zoomRange;
+            var date = new Date();
+            var curDate = null;
 
-            gameplayMap.setScale(this.currentScale);
-            this.timeLeft--;
-
-
-            if(this.timeLeft==0)
-                this.zoomActivate=false;
+            do { curDate = new Date(); }
+            while(curDate-date < this.timeZoom);
+            this.zoomActivate=false;
         }
 
-        return this.zoomActivate;
-    },
+        if(this.currentScale<1)
+        {
+            this.currentScale+=this.zoomRange;
+            gameplayMap.setScale(this.currentScale);
+            return true;
+        }else
+            return false;
 
-    //Permite resetear el zoom
-    //una vez acabado el zoom, se debe usar esta variable para poder ejecutar
-    //nuevamente el efecto
-    resetZoom:function(){
-
-        this.timeLeft=this.timeZoom;
-        zoomActivate=true;
-    },
-
-    changeTypeZoom:function(type_Zoom)
-    {
-        this.typeZoom=type_Zoom;
     },
 
     zoomRange:0.01,
     typeZoom:1,
     scaleInit:1,
     currentScale:0.1,
-    timeZoom:180,
+    timeZoom:1800,
     timeLeft:180,
 
     zoomActivate:true,
-
 }
-
-
-
 
 var HelloWorldScene = cc.Scene.extend({
     gameplayLayer : null,
--- src/powerups.js
+++ src/powerups.js
@@ -13,7 +13,7 @@ function executePowerup(tile){
             //TODO
             break;
         case '1':
-            //TODO
+            clockController.activateClock();
             break;
         case '2':
             TorchController.activateTorch();
@@ -26,6 +26,38 @@ function executePowerup(tile){
     powerupLayer.setTileGID(0,tile.x,tile.y);
     powerupLayer.removeTileAt(cc.p(tile.x,tile.y));
 }
+
+//objetivo: relantizar el objeto monstruo un tiempo n en milisengundos
+var clockController = {
+
+    activateClock: function(){
+        this.flag_avtivatePowerUp=true;
+    },
+
+    getSpeed: function(){
+
+        if(this.flag_avtivatePowerUp)
+        {
+            this.currentSpeed = this.monster_speed*(1-this.porcentajeDis/100);
+            this.currentTime--;
+            if(this.currentTime==0)
+            {
+                this.currentTime= this.timeMiliseg;
+                this.flag_avtivatePowerUp=false;
+                this.currentSpeed = this.monster_speed;
+            }
+        }
+
+        return this.currentSpeed;
+    },
+
+    flag_avtivatePowerUp:false,
+    timeMiliseg:200,
+    currentTime:200,
+    porcentajeDis:30,//Porcentaje de la velocidad que se disminuirá del monstruo
+    monster_speed:0.75,
+    currentSpeed:0.75,
+};
 
 var TorchController = (function(){
     //Variables de antorcha
